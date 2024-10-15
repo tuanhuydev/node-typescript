8aa8e6cd7cdc5b82995da2f112879aa504217117
tuanhuydev <tuanhuydev@gmail.com>
2024-10-15 22:08:33 +0700

    feat(TICKET-1): implement feature A

--- app.ts
+++ app.ts
@@ -1,11 +1,1 @@
-import { readHome } from './src/nodejs/file';
-import * as http from "http";
-
-const server = http.createServer((req, res) => {
-	readHome.then((data) => {
-		console.log('data', data);
-		res.setHeader('Content-Type', 'text/html');
-		return res.end(data);
-	});
-});
-server.listen(3000, () => { console.log('Server is running on port 3000') });
+import "./src/patts/abstractFactory";
--- src/patts/abstractFactory.ts
+++ src/patts/abstractFactory.ts
@@ -1,76 +1,104 @@
-interface StorageInterface {
-	save: (file: File) => void;
+// Abstract Factory Pattern
+interface DatabaseFactory {
+	createConnection(): ConnectionHandler;
+	buildQuery(): QueryBuilderHandler;
 }
 
-type VendorStorageType = "s3" | "azule";
-type StorageType = VendorStorageType & "local";
-
-class DiskStorage implements StorageInterface {
-	constructor() {}
-
-	save(file: File) {
-		console.log(`Save file ${file.name} to local disk`);
-	}
+interface ConnectionHandler {
+	connect(): void;
 }
 
-class S3Storage implements StorageInterface {
-	constructor() {}
+interface QueryBuilderHandler {
+	build(query: string): string;
+}
 
-	save(file: File) {
-		console.log(`Save file ${file.name} to S3`);
+class MongoConnection implements ConnectionHandler {
+	connect(): void {
+		console.log("Connect to MongoDB");
 	}
 }
 
-class AzuleBlob implements StorageInterface {
-	constructor() {}
-
-	save(file: File) {
-		console.log(`Save file ${file.name} to Azule Blob`);
+class MongoQueryBuilder implements QueryBuilderHandler {
+	build(query: string): string {
+		return "MongoDB build " + query;
 	}
 }
 
-class VendorStorageFactory {
-	#s3Storage: DiskStorage;
-	#azuleStorage: AzuleBlob;
-
-	constructor() {
-		this.#s3Storage = new S3Storage();
-		this.#azuleStorage = new AzuleBlob();
-	}
-
-	save(type: VendorStorageType, file: File) {
-		switch (type) {
-			case "s3":
-				this.#s3Storage.save(file);
-				break;
-			case "azule":
-				this.#azuleStorage.save(file);
-			default:
-				throw new Error("Save type doesn't support");
-		}
+class MySQLConnection implements ConnectionHandler {
+	connect(): void {
+		console.log("Connect to MySQL");
 	}
 }
 
-class StorageFactory {
-	#localStorage: DiskStorage;
-	#venderStorage: VendorStorageFactory;
-
-	constructor() {
-		this.#localStorage = new DiskStorage();
-		this.#venderStorage = new VendorStorageFactory();
-	}
-
-	save(storageType: StorageType, file: File) {
-		switch (storageType) {
-			case "s3":
-				this.#venderStorage.save("s3", file);
-				break;
-			case "azule":
-				this.#venderStorage.save("azule", file);
-				break;
-			default:
-				this.#localStorage.save(file);
-				break;
-		}
+class MySQLQueryBuilder implements QueryBuilderHandler {
+	build(query: string): string {
+		return "MySQL build " + query;
 	}
 }
+
+// Factory
+
+class MongoFactory implements DatabaseFactory {
+	buildQuery(): QueryBuilderHandler {
+		return new MongoQueryBuilder();
+	}
+	createConnection(): ConnectionHandler {
+		return new MongoConnection();
+	}
+}
+
+
+class MySQLFactory implements DatabaseFactory {
+	buildQuery(): QueryBuilderHandler {
+		return new MySQLQueryBuilder();
+	}
+	createConnection(): ConnectionHandler {
+		return new MySQLConnection();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Application {
+	private connectionHandler: ConnectionHandler;
+	private queryBuilderHandler: QueryBuilderHandler;
+
+	constructor(factory: DatabaseFactory) {
+		this.connectionHandler = factory.createConnection();
+		this.queryBuilderHandler = factory.buildQuery();
+	}
+
+	run(query: string) {
+		this.connectionHandler.connect();
+		const builtQuery: string = this.queryBuilderHandler.build(query);
+		console.log("Run ", builtQuery);
+	}
+}
+
+
+const mongoFactory = new MongoFactory();
+const mongoApp = new Application(mongoFactory);
+
+mongoApp.run("Select * from users");
+
+const mySQLFactory = new MySQLFactory();
+const mySQLApp = new Application(mySQLFactory);
+
+mySQLApp.run("Select * from users");
